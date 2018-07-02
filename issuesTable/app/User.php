c06af35f282900bc2202af9c0dd8c7af76ceb676
A1exMorris <wertava@gmail.com>
2018-07-03 02:08:07 +0300

    issueTable laravel

--- issuesTable/app/User.php
+++ issuesTable/app/User.php
@@ -5,6 +5,20 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $password
+ * @property \Carbon\Carbon|null $created_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUsername($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable;
--- issuesTable/resources/views/welcome.blade.php
+++ issuesTable/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    MyFirstLaravel
                 </div>
 
                 <div class="links">
